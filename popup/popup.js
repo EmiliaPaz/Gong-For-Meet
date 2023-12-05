a8398a5de5f1b6f6f80100d17721be47c7d105ef
EmiliaPaz <emipazojeda@gmail.com>
2023-12-04 22:22:15 -0800

    Add css for widget and also use it for input. Input is not connected to real countdown

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,12 @@
-// DOM elements.
+// Buttons.
 let startButton = document.getElementById("startButton");
 let stopButton = document.getElementById("stopButton");
-let countdown = document.getElementById("countdown");
+
+// Countdown.
+let minutes = document.getElementById("minutes");
+let seconds = document.getElementById("seconds");
+const countdownInputs = document.querySelectorAll("input");
+const countdownError = document.querySelector("#error");
 
 let countdownInterval;
 
@@ -10,11 +15,26 @@ const CountdownState = {
   notRunning: 1,
 };
 
+// Shows an error when countdown input is invalid.
+const throwCountdownErr = (mssg, input, invalid = false) => {
+  // control display of error
+  if (invalid === true) {
+    input.classList.add("invalid");
+    countdownError.style.visibility = "visible";
+    countdownError.innerHTML = `<small>${mssg}</small>`;
+  } else {
+    input.classList.remove("invalid");
+    countdownError.style.visibility = "hidden";
+  }
+};
+
+// Resets the countdown interval and stored value.
 function resetCountdown() {
   clearInterval(countdownInterval);
   chrome.storage.session.clear();
 }
 
+// Updates the countdown UI every minute until it hits the `endTime`.
 function updateCountdownUI(endTime) {
   // Update countdown every 1s.
   countdownInterval = setInterval(function () {
@@ -31,33 +51,86 @@ function updateCountdownUI(endTime) {
       return;
     }
 
-    let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+    let remainingMinutes = Math.floor(
+      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    let remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    // Add leading zero if needed.
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    // Display the countdown
-    countdown.innerHTML = minutes + ":" + seconds;
+    minutes.value = minutes < 10 ? "0" + remainingMinutes : remainingMinutes;
+    seconds.value = seconds < 10 ? "0" + remainingSeconds : remainingSeconds;
   }, 1000);
 }
 
+// Updates the popupo for `states`. `endTime` is only given when the states is
+// `notRunning`.
 function updateUI(state, endTime) {
   switch (state) {
     case CountdownState.running:
       startButton.style.display = "none";
       stopButton.style.display = "block";
-      countdown.innerHTML = updateCountdownUI(endTime);
+      updateCountdownUI(endTime);
+      countdownInputs.forEach((input) => {
+        input.disabled = true;
+      });
       break;
     case CountdownState.notRunning:
       startButton.style.display = "block";
       stopButton.style.display = "none";
-      countdown.innerHTML = "";
+      countdownInputs.forEach((input) => {
+        input.disabled = false;
+        input.value = 0;
+      });
       break;
   }
 }
 
+// Recieve countdown inputs.
+countdownInputs.forEach((input) => {
+  let isInvalid = false;
+  let mssg;
+
+  input.addEventListener("focus", (e) => {
+    // Blank input on default values
+    if (e.target.value === "0" || e.target.value === "00") {
+      e.target.value = "";
+    }
+
+    // Show error mssg for invalid inputs when not corrected
+    if (e.target.classList.contains("invalid")) {
+      isInvalid = true;
+      throwCountdownErr(mssg, input, isInvalid);
+    } else {
+      isInvalid = false;
+      throwCountdownErr(mssg, input);
+    }
+  });
+
+  input.addEventListener("keyup", (e) => {
+    let val = e.target.value;
+
+    // Only allow digits.
+    input.value = val.replace(/[^0-9]+/g, "");
+
+    // Restrict input characters
+    if (e.target.value.length > 2) {
+      input.value = e.target.value.substring(0, 2);
+    }
+
+    // Max amount of seconds and minutes.
+    const max = 60;
+    if (+input.value > max) {
+      isInvalid = true;
+      mssg = `Value can't be more than ${max}`;
+
+      throwCountdownErr(mssg, input, isInvalid);
+    } else {
+      isInvalid = false;
+      throwCountdownErr(mssg, input);
+    }
+  });
+});
+
+// Start countdown when start button is clicked.
 startButton.addEventListener("click", async () => {
   // TODO: Add textbox so user can select countdown time.
   let countdownTime = 1; // in minutes
@@ -78,6 +151,7 @@ startButton.addEventListener("click", async () => {
   });
 });
 
+// Stop countdown when stopn button is clicked.
 stopButton.addEventListener("click", async () => {
   resetCountdown();
   updateUI(CountdownState.notRunning);
@@ -89,7 +163,8 @@ stopButton.addEventListener("click", async () => {
   });
 });
 
-chrome.storage.session.get(["endTime"], function (result) {
+// Retrieve stored end time and update value according to state.
+chrome.storage.session.get(["endTime"], (result) => {
   const currentTime = new Date();
   const endTime = result && result.endTime ? new Date(result.endTime) : null;
 
